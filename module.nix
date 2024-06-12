caa64cea47b07403f0a530acfd410c3241b1f94b
Fea <feathecutie@disroot.org>
2024-06-12 13:06:19 +0200

    Add simple reverse proxy (caddy & nginx)

--- module.nix
+++ module.nix
@@ -27,16 +27,38 @@ in
         createLocally = lib.mkOption {
           type = lib.types.bool;
           default = false;
-          description = "Create the PostgreSQL database locally. This overrides the `db` key of the misskey configuration.";
+          description = "Create the PostgreSQL database locally. Overrides `settings.db.{db,host,port,user,pass}`.";
         };
       };
       redis = {
         createLocally = lib.mkOption {
           type = lib.types.bool;
           default = false;
-          description = "Create and use a local Redis instance. This overrides the `redis` key of the misskey configuration.";
+          description = "Create and use a local Redis instance. Overrides `settings.redis.host`";
         };
-        port = lib.mkOption { };
+        # pubSub.createLocally = lib.mkOption {
+        #   type = lib.types.bool;
+        #   default = false;
+        #   description = "Create and use a local Redis instance. Overrides `settings.redisForPubSub.host`";
+        # };
+        # jobQueue.createLocally = lib.mkOption {
+        #   type = lib.types.bool;
+        #   default = false;
+        #   description = "Create and use a local Redis instance. Overrides `settings.redisForJobQueue.host`";
+        # };
+        # timelines.createLocally = lib.mkOption {
+        #   type = lib.types.bool;
+        #   default = false;
+        #   description = "Create and use a local Redis instance. Overrides `settings.redisForTimelines.host`";
+        # };
+      };
+      reverseProxy = {
+        enable = lib.mkEnableOption "a HTTP proxy for Misskey";
+        webserver = lib.mkOption {
+          type = lib.types.enum [ "caddy" /*"nginx"*/ ];
+          default = "caddy";
+          description = "The webserver to use as a reverse proxy.";
+        };
       };
     };
   };
@@ -47,10 +69,10 @@ in
       wants = [ "network-online.target" ];
       wantedBy = [ "multi-user.target" ];
       environment = {
-        MISSKEY_CONFIG_YML = settingsFormat.generate "misskey-config.yml" (
-          lib.recursiveUpdate
-            (lib.recursiveUpdate cfg.settings (lib.optionalAttrs cfg.database.createLocally {
-              db = {
+        MISSKEY_CONFIG_YML = settingsFormat.generate "misskey-config.yml"
+          (
+            lib.recursiveUpdate cfg.settings {
+              db = lib.optionalAttrs cfg.database.createLocally {
                 db = "misskey";
                 # Use unix socket instead of localhost to allow PostgreSQL peer authentication,
                 # required for `services.postgresql.ensureUsers`
@@ -59,21 +81,17 @@ in
                 user = "misskey";
                 pass = null;
               };
-            }))
-            (lib.optionalAttrs cfg.redis.createLocally {
-              redis = {
+              redis = lib.optionalAttrs cfg.redis.createLocally {
                 host = "localhost";
-                port = 6379;
               };
-            })
-        );
+            }
+          )
+        ;
       };
       serviceConfig = {
         ExecStart = "${cfg.package}/bin/misskey migrateandstart";
         StateDirectory = "misskey";
         StateDirectoryMode = "700";
-        # RuntimeDirectory = "misskey";
-        # RuntimeDirectoryMode = "700";
         TimeoutSec = 60;
         DynamicUser = true;
         User = "misskey";
@@ -111,9 +129,29 @@ in
     services.redis.servers = lib.mkIf cfg.redis.createLocally {
       misskey = {
         enable = true;
-        port = 6379;
+        port = cfg.settings.redis.port;
       };
     };
+
+    services.caddy = lib.mkIf (cfg.reverseProxy.enable && cfg.reverseProxy.webserver == "caddy") {
+      enable = true;
+      virtualHosts.${cfg.settings.url} = {
+        extraConfig = ''
+          reverse_proxy localhost:${toString cfg.settings.port}
+        '';
+      };
+    };
+
+    # services.nginx = lib.mkIf (cfg.reverseProxy.enable && cfg.reverseProxy.webserver == "nginx") {
+    #   enable = true;
+    #   virtualHosts.${cfg.settings.url} = {
+    #     enableACME = true;
+    #     locations."/" = {
+    #       proxyPass = "http://127.0.0.1:${toString cfg.settings.port}";
+    #       recommendedProxySettings = true;
+    #     };
+    #   };
+    # };
   };
 }
 
